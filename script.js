e4e3b2b1b265b0eafa10971c244950ad5309fc97
sithsiri <sith@live.com>
2020-09-02 15:11:09 -0500

    Update: JavaScript is the bane of my existence

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function romajify(input, output) {
 	let out = input.value;
 	for (let i = 0; i < hiragana.length; i++) {
-		out.replaceAll(hiragana[i][0], hiragana[i][1]);
+		out = out.replaceAll(hiragana[i][0], hiragana[i][1]);
 	}
 	output.innerHTML = out;
 }
 
-// https://stackoverflow.com/questions/2116558/fastest-method-to-replace-all-instances-of-a-character-in-a-string
-String.prototype.replaceAll = function(str1, str2, ignore) 
+// https://stackoverflow.com/a/6714233/5155509
+String.prototype.replaceAll = function(str1, str2, ignore)
 {
 	return this.replace(
 		new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),
 		(ignore?"gi":"g")),
 		(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2
 	);
-} 
+}

28e1be2fc3148286da093eb84771db5b57b05aaa
sithsiri <sith@live.com>
2020-09-02 15:00:24 -0500

    bodge?

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function romajify(input, output) {
 	let out = input.value;
 	for (let i = 0; i < hiragana.length; i++) {
-		out.replace(hiragana[i][0], hiragana[i][1]);
+		out.replaceAll(hiragana[i][0], hiragana[i][1]);
 	}
 	output.innerHTML = out;
 }
+
+// https://stackoverflow.com/questions/2116558/fastest-method-to-replace-all-instances-of-a-character-in-a-string
+String.prototype.replaceAll = function(str1, str2, ignore) 
+{
+	return this.replace(
+		new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),
+		(ignore?"gi":"g")),
+		(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2
+	);
+} 

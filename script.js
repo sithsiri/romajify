87b8eb879b96174deea0a351e3e8d924ec6147a4
sithsiri <sith@live.com>
2020-09-02 14:50:23 -0500

    "that's a comma Sithindu"

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function romajify(input, output) {
 	let out = input.value;
-	for (let i = 0; i < hiragana.length, i++) {
+	for (let i = 0; i < hiragana.length; i++) {
 		out.replace(hiragana[i][0], hiragana[i][1]);
 	}
 	output.innerHTML = out;
